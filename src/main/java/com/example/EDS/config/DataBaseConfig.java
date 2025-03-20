3f5907c3b4e875a1ef502d3c8ba8038e6be3b54d
Gabriel Expedito <gabrielexpedi18@gmail.com>
2025-03-20 20:26:16 -0300

    Data Source Config

---
+++ src/main/java/com/example/EDS/config/DataBaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.EDS.config;
+
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url("jdbc:postgresql://localhost:5432/postgres")
+                .username("postgres")
+                .password("12345")
+                .build();
+    }
+
+}
